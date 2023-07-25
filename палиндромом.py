d684e64150c319fbd350acc1be8f288c1d0050eb
Bubb1eGum1 <140273823+Bubb1eGum1@users.noreply.github.com>
2023-07-25 16:28:42 +0500

    Update палиндромом.py

--- палиндромом.py
+++ палиндромом.py
@@ -1,5 +1,7 @@
-word = input()
-if word == word[::-1]:
-    print('слово является палиндромом')
-else:
-    print('слово не является палиндромом')
+def polidrome(word):
+    if word == word[::-1]:
+        return True
+    else:
+        return False
+
+print(polidrome('сос'))
